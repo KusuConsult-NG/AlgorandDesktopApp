fcc1d72e8c66f10586ecb6ce1d5774ad90903155
Sethmichael01 <sethmichael1.sm@gmail.com>
2020-11-21 18:30:52 +0100

    Initial Commit

---
+++ DesktopAlgorandApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Algorand;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopAlgorandApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Account account = new Account();
+            var displayAddress = account.Address.ToString();
+            var accountmnemonic = account.ToMnemonic();
+            label2.Text = displayAddress;
+            textBox1.Text = displayAddress;
+            textBox2.Text = accountmnemonic;
+        }
+    }
+}
